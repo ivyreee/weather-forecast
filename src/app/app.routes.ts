79be10b545222b7f34bc67262a79b480104c7672
ivyreee <ivy@drive.digital>
2024-04-13 17:16:16 +0800

    Adding basic routing, creating home and landing page components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: '/landing-page', pathMatch: 'full' },
+    { path: 'landing-page', component: LandingPageComponent },
+    { path: 'home', component: HomeComponent }
+];
