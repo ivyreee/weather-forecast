b347aa90a82daad17f183884060dacb3fa46a1d6
ivyreee <ivy@drive.digital>
2024-04-13 18:46:02 +0800

    Added routing to weather and home pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { HomeComponent } from './home/home.component';
+import { WeatherComponent } from './weather/weather.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/landing-page', pathMatch: 'full' },
     { path: 'landing-page', component: LandingPageComponent },
-    { path: 'home', component: HomeComponent }
+    { path: 'home', component: HomeComponent },
+    { path: 'weather', component: WeatherComponent }
 ];
--- src/app/weather/weather.component.ts
+++ src/app/weather/weather.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-weather',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './weather.component.html',
   styleUrl: './weather.component.scss'
 })
